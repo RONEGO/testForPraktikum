7cfea8f8c68189f459f04f46bd4e1ee332831f9d
Yegor iOS Geronin <geronin.yegor@gmail.com>
2023-10-06 09:48:49 +0300

    PKM: Comments for code

--- CalendarViewController.swift
+++ CalendarViewController.swift
@@ -1,9 +1,17 @@
+// MARK: - Сделал ревью кода в виде комментов, потому что обычные комменты не видно в публичном пр
+
 import UIKit
 
 final class CalendarViewController: UIViewController {
 	
 	let calendarViewModel = CalendarViewModel()
 	
+//	В отличие от IBOutlets, тут нам не нужно использовать forceUnwrap.
+//	Давай попробуем 2 варианта:
+//
+//	- Перепишем все на lazy var
+//	- Сделаем let переменную и все конфигурацию вынесем в отдельную функцию
+//	forceUnwrap - особенно internal - 99.9% приведет к крашу.
 	var scrollView: UIScrollView!
 	var containerView: View!
 	var alisaCollectionView: UICollectionView!
@@ -12,27 +20,39 @@ final class CalendarViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+//		Создаем memory leak!
+//		[weak self] in нужно добавить в кложуру
+//		Почитай, пожалуйста, про strong reference cycle и ARC - если будут вопросы, можешь задать в личку.
 		DispatchQueue.main.async {
 			self.makeUI()
 		}
+//		Конфигурация модельки происходит после makeUI.
+//		Мы точно готовы к такому поведению? Может быть, стоит попробовать сначала приготовить вью, а потом - apply-ить к ней модельку?
 		calendarViewModel.configureMembersCalendar()
 	}
 }
 	
 // MARK: - UICollectionViewDelegateFlowLayout, UICollectionViewDataSource
 extension CalendarViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+//	Если мы делаем MVVM, то почему Controller берет на себя роль сущности, которая решает количество ячеек (тем более константно) - давай вынесем это в Model сущность, и количество будет вычиститься на основе array.
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 		11
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//		Не видел, чтобы мы регистрировали ячейку для нашей коллекции - словим рантайм краш :(
 		guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AssistantCollectionViewCell.cellID, for: indexPath) as? AssistantCollectionViewCell else { return UICollectionViewCell() }
 		
+//		Почитай про DRY
+//		Давай сначала делать все ячейки по дефолту серыми, а по итогам конфигурации, если нужно - будем менять.
 		if collectionView == alisaCollectionView {
 			cell.makeBackgorundColor(color: .systemBlue)
 			if !calendarViewModel.alicesSortedArray.contains(calendarViewModel.hours[indexPath.row]) {
 				cell.makeBackgorundColor(color: .systemGray)
 			}
+
+//			Есть принципиальная разница между == и ===
+//			Вполне возможно, что система решит, что все эти коллекции равны по своим внутренностям, но они в нашей логики не одинаковы. Поэтому правильней было бы их сравнивать по pointer-у.
 		} else if collectionView == bobCollectionView {
 			cell.makeBackgorundColor(color: .systemOrange)
 			if !calendarViewModel.bobsSortedArray.contains(calendarViewModel.hours[indexPath.row]) {
--- CalendarViewModel.swift
+++ CalendarViewModel.swift
@@ -1,13 +1,21 @@
 import Foundation
 
+//Протокол никогда не был использован!
+//Проверь, пожалуйста, CalendarViewController.
+//Ты кастишь там проперти напрямую при создании, а надо указать тип как протокол.
 protocol CalendarViewModelProtocol: AnyObject {
 	func configureMembersCalendar()
 }
 
 final class CalendarViewModel: CalendarViewModelProtocol {
 	
+//	Давай сделаем поля приватными.
+//	И если они где-то понадобятся как { get } - закроем протоколом.
 	var alicesSortedArray = [Int]()
 	var bobsSortedArray = [Int]()
+
+//	let hours = Array(9...19)
+//	это будет потом сильно проще менять ;)
 	let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 	
 	let aliceCalendar = [
@@ -27,6 +35,11 @@ final class CalendarViewModel: CalendarViewModelProtocol {
 		configureBobCalendar()
 	}
 	
+//	Давай представим, что я меняю у aliceCalendar первый BookedTime и второй местами.
+//	Твой алгоритм сломается, ведь часы уже будут не отсортированы.
+//	Можно попробовать
+//	- сортировать [BookedTime] по startTime в начале функции
+//	- отсортировать сам массив в конце метода
 	private func configureAliceCalendar() {
 		for busynessHour in aliceCalendar {
 			for i in busynessHour.start ..< busynessHour.end {
